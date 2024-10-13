f5ca1fb13e4dbf7389c05868dd2939c37a643c43
Matt Kuhn <mlkkuhn@live.com>
2024-10-13 09:43:18 -0400

    menu get it's own component as well

--- index.php
+++ index.php
@@ -13,17 +13,7 @@
 </head>
 
 <body>
-    <h1>Welcome!</h1>
-    <ul class="menu">
-        <li><a class="btn btn-med btn-link" href="#pray-tab" hx-get="/pray.php" hx-target="#pray-tab">Pray</a></li>
-        <li><a class="btn btn-med btn-link" href="#prayer-publish-tab" hx-get="/prayer-publish.php" hx-target="#prayer-publish-tab">Publish Prayer</a></li>
-    </ul>
-
-    <div id="pray-tab" class="tab-content">
-        <p class="htmx-indicator">Result loading...</p>
-    </div>
-
-    <div id="prayer-publish-tab" class="tab-content">
+    <div id="menu" hx-get="/menu.php" hx-trigger="load">
         <p class="htmx-indicator">Result loading...</p>
     </div>
 </body>
---
+++ menu.php
@@ -0,0 +1,15 @@
+
+<link rel="stylesheet" href="/menu.css">
+<h1>Welcome!</h1>
+<ul class="menu">
+    <li><a class="btn btn-med btn-link" href="#pray-tab" hx-get="/pray.php" hx-target="#pray-tab">Pray</a></li>
+    <li><a class="btn btn-med btn-link" href="#prayer-publish-tab" hx-get="/prayer-publish.php" hx-target="#prayer-publish-tab">Publish Prayer</a></li>
+</ul>
+
+<div id="pray-tab" class="tab-content">
+    <p class="htmx-indicator">Result loading...</p>
+</div>
+
+<div id="prayer-publish-tab" class="tab-content">
+    <p class="htmx-indicator">Result loading...</p>
+</div>
