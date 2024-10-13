60991a0193357d6afcc74924851d0636f9083d82
Matt Kuhn <mlkkuhn@live.com>
2024-10-13 10:06:02 -0400

    menu big butt(ons)

--- menu.php
+++ menu.php
@@ -1,15 +1,6 @@
 
 <link rel="stylesheet" href="/menu.css">
-<h1>Welcome!</h1>
-<ul class="menu">
-    <li><a class="btn btn-med btn-link" href="#pray-tab" hx-get="/pray.php" hx-target="#pray-tab">Pray</a></li>
-    <li><a class="btn btn-med btn-link" href="#prayer-publish-tab" hx-get="/prayer-publish.php" hx-target="#prayer-publish-tab">Publish Prayer</a></li>
+<ul class="menu" id="menu">
+    <li><a class="menu-item" hx-get="/pray.php" hx-target="#menu">Pray</a></li>
+    <li><a class="menu-item" hx-get="/prayer-publish.php" hx-target="#menu">Publish Prayer</a></li>
 </ul>
-
-<div id="pray-tab" class="tab-content">
-    <p class="htmx-indicator">Result loading...</p>
-</div>
-
-<div id="prayer-publish-tab" class="tab-content">
-    <p class="htmx-indicator">Result loading...</p>
-</div>
