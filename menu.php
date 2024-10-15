865dc466195578f2daa61695c099cc58591dae80
Kuhn, Matthew L <mlkuhn@tva.gov>
2024-10-15 13:49:05 -0400

    menu css

--- menu.php
+++ menu.php
@@ -1,5 +1,3 @@
-
-<link rel="stylesheet" href="/menu.css">
 <ul class="menu" id="menu">
     <li><a class="menu-item" hx-get="/prayer-search.php" hx-target="#menu">Pray</a></li>
     <li><a class="menu-item" hx-get="/prayer-publish.php" hx-target="#menu">Publish</a></li>
