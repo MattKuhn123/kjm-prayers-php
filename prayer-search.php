79793d356d489ad40eda274879539c4c23d6bb68
Kuhn, Matthew L <mlkuhn@tva.gov>
2024-10-15 16:40:14 -0400

    prepopulate query date range

--- prayer-search.php
+++ prayer-search.php
@@ -19,7 +19,7 @@
 
     <fieldset id="fieldset-query-name">
         <label for="query-name">Is there a person you'd like to pray for?</label>
-        <input value="<?= !empty($_GET["query-name"]) ? $_GET["query-name"] : "" ?>" id="query-name" name="query-name" type="search" />
+        <input id="query-name" name="query-name" type="search" placeholder="Name" />
         <a href="#fieldset-query-county" class="btn btn-primary btn-fat">Continue</a>
     </fieldset>
 
@@ -27,19 +27,19 @@
         <label for="query-county">Is there a county you'd like to pray for?</label>
         <select id="query-county" name="query-county">
             <option></option>
-            <option <?php if (!empty($_GET["query-county"]) && $_GET["query-county"] === "Boone") echo ("selected"); ?>>Boone</option>
-            <option <?php if (!empty($_GET["query-county"]) && $_GET["query-county"] === "Campbell") echo ("selected"); ?>>Campbell</option>
-            <option <?php if (!empty($_GET["query-county"]) && $_GET["query-county"] === "Kenton") echo ("selected"); ?>>Kenton</option>
-            <option <?php if (!empty($_GET["query-county"]) && $_GET["query-county"] === "Grant") echo ("selected"); ?>>Grant</option>
+            <option>Boone</option>
+            <option>Campbell</option>
+            <option>Kenton</option>
+            <option>Grant</option>
         </select>
         <a href="#fieldset-query-date-start" class="btn btn-primary btn-fat">Continue</a>
     </fieldset>
 
     <fieldset id="fieldset-query-date-start">
         <label for="query-date-start">Are there prayers that were published in a certain time frame that you'd like to pray for?</label>
-        <input value="<?= !empty($_GET["query-date-start"]) ? $_GET["query-date-start"] : "" ?>" id="query-date-start" name="query-date-start" type="date" />
+        <input id="query-date-start" name="query-date-start" type="date" value="<?php echo date('Y-m-d', strtotime('-31 days')); ?>" />
         -
-        <input value="<?= !empty($_GET["query-date-end"]) ? $_GET["query-date-end"] : "" ?>" id="query-date-end" name="query-date-end" type="date" />
+        <input id="query-date-end" name="query-date-end" type="date" value="<?php echo date('Y-m-d'); ?>" />
         <input class="btn btn-primary btn-fat" type="submit" value="Search" />
     </fieldset>
 
