fa649b44a4500e3345dd794e7bf19f22db4db58d
Kuhn, Matthew L <mlkuhn@tva.gov>
2024-10-22 13:50:16 -0400

    tabs(?)

--- prayer-search.php
+++ prayer-search.php
@@ -23,6 +23,13 @@ if (!$is_logged_in) {
 
 <body class="centered">
     <form class="my-form" action="/prayer-results.php" id="prayer-search" method="GET">
+        <ol class="my-tab-list">
+            <li><a class="my-tab-list-item" href="#fieldset-query-start">Start</a></li>
+            <li><a class="my-tab-list-item" href="#fieldset-query-name">Name</a></li>
+            <li><a class="my-tab-list-item" href="#fieldset-query-county">County</a></li>
+            <li><a class="my-tab-list-item" href="#fieldset-query-date">Date</a></li>
+        </ol>
+
         <fieldset class="my-fieldset" id="fieldset-query-start">
             <label for="query-start">Would you like to see the latest prayers? Or are you looking for something specific?</label>
             <input class="btn btn-primary my-input" type="submit" value="Take me to the latest prayers!" />
@@ -33,7 +40,10 @@ if (!$is_logged_in) {
         <fieldset class="my-fieldset" id="fieldset-query-name">
             <label for="query-name">Is there a person you'd like to pray for?</label>
             <input class="my-input" id="query-name" name="query-name" type="search" placeholder="Name" />
-            <a href="#fieldset-query-county" class="btn btn-primary">Continue</a>
+            <section class="actions">
+                <a href="#fieldset-query-county" class="btn btn-secondary">Next</a>
+            </section>
+            <input class="btn btn-primary my-input" type="submit" value="Search" />
         </fieldset>
 
         <fieldset class="my-fieldset" id="fieldset-query-county">
@@ -45,14 +55,21 @@ if (!$is_logged_in) {
                 <option>Kenton</option>
                 <option>Grant</option>
             </select>
-            <a href="#fieldset-query-date-start" class="btn btn-primary">Continue</a>
+            <section class="actions">
+                <a href="#fieldset-query-name" class="btn btn-secondary">Back</a>
+                <a href="#fieldset-query-date" class="btn btn-secondary">Next</a>
+            </section>
+            <input class="btn btn-primary my-input" type="submit" value="Search" />
         </fieldset>
 
-        <fieldset class="my-fieldset" id="fieldset-query-date-start">
+        <fieldset class="my-fieldset" id="fieldset-query-date">
             <label for="query-date-start">Are there prayers that were published in a certain time frame that you'd like to pray for?</label>
             <input class="my-input" id="query-date-start" name="query-date-start" type="date" value="<?php echo date('Y-m-d', strtotime('-200 days')); ?>" />
             -
             <input class="my-input" id="query-date-end" name="query-date-end" type="date" value="<?php echo date('Y-m-d'); ?>" />
+            <section class="actions">
+                <a href="#fieldset-query-county" class="btn btn-secondary">Back</a>
+            </section>
             <input class="btn btn-primary my-input" type="submit" value="Search" />
         </fieldset>
 
